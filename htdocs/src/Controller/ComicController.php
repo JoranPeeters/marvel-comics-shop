10844d1fca316a0870c79dc9364822a4e12d167d
JoranPeeters <joran.peeters@student.kdg.be>
2024-11-11 19:26:09 +0100

    MARVEL_COMICS_SHOP_008: Remove: Removed "," in ComicController  constructor

--- htdocs/src/Controller/ComicController.php
+++ htdocs/src/Controller/ComicController.php
@@ -5,13 +5,12 @@ namespace App\Controller;
 use App\Repository\ComicRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ComicController extends AbstractController
 {
     public function __construct(
-        private readonly ComicRepository $comicRepository,
+        private readonly ComicRepository $comicRepository
     ) {}
 
     #[Route('/comic/{id}', name: 'app_comic_detail')]
